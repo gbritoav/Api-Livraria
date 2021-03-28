e8a262c4a2e8b1424d4dd01472090592a4d90c7e
Gabriel Brito <gabrielbrito@hubert.com.br>
2021-03-28 20:20:28 -0300

    teste de retorno

--- Livraria/Controllers/TestesController.cs
+++ Livraria/Controllers/TestesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Livraria.Model;
+using Livraria.Repositorio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Livraria.Controllers
@@ -11,13 +13,22 @@ namespace Livraria.Controllers
     public class TestesController : Controller
     {
 
+        private readonly AutorRepositorio _contextoAutor = new AutorRepositorio();
+
+        public TestesController(Contexto contexto)
+        {
+            _contextoAutor.Contexto(contexto);
+        }
+
+
         /// <summary>
         /// Teste
         /// </summary>
         [HttpGet]
         public async Task<ActionResult<string>> GetTeste()
         {
-            string teste = "Bem vindo!";
+            var teste2 = _contextoAutor.GetAll();
+            string teste = "Bem vindo " + teste2.First().Nome;
             return new JsonResult(teste);
         }
 
